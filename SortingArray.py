3b77d0b653ccb5521b971c449db988dea41819a2
vonnkee13 <65433415+vonnkee13@users.noreply.github.com>
2021-09-24 21:24:26 -0400

    Update and rename README.md to SortingArray.py
    This is a python code for sorting n numbers of a sequence in increasing order. The code is divided into two steps. The first step is for each even indexed element (Python is index 0), swap the element and the number to its right, whenever the element is greater. The second step is for each odd indexed element, swap the element and the number to its right, whenever the element is greater. Repeat the the above two steps until no more swapping occurs.

---
+++ SortingArray.py
@@ -0,0 +1,13 @@
+# SortingArray
+This is a python code for sorting n numbers of a sequence in increasing order.
+def Sort(seq): #sorts an array of n elements
+    n=len(seq)
+    stop=1
+    while stop==1:
+        stop=0
+        for j in range(2):
+            for i in range(0,n-1-j,2):
+                    if seq[i+j]>seq[i+j+1]:
+                        seq[i+j], seq[i+j+1] = seq[i+j+1], seq[i+j]
+                        stop=1
+    return seq
